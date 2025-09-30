c9e2b53f5cc11081323573119fffc386ef65e9b5
MAHDI-EIVAZI <mahdieivazi.1397@gmail.com>
2025-09-30 18:31:00 +0330

    Add Application DbContext

---
+++ ExamProject/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ExamProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamProject
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Exam> exams { get; set; }
+        public DbSet<ExamGroup> examGroups { get; set; }
+        public DbSet<ExamStudent> examStudents { get; set; }
+        public DbSet<Questions> questions { get; set; }
+        public DbSet<Student> students { get; set; }
+        public DbSet<StudentAsnwers> studentsAsnwers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
